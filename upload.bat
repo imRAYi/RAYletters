7bf3c27b3394076afc552c4492fe6ac39ecb84e9
imRAYi <rayyan.khaled999@gmail.com>
2026-02-27 23:09:36 +0300

    Auto-update from Rayan Tool

--- upload.bat
+++ upload.bat
@@ -1,6 +1,12 @@
 @echo off
+:: تنظيف الشاشة
+cls
+echo 🚀 Starting Rayan's Auto-Upload...
+
+:: تنفيذ أوامر الـ Git
 git add .
 git commit -m "Auto-update from Rayan Tool"
 git push origin main
+
 echo ✨ DONE! Rayletters.life is now updating...
 pause
