f17653773c219a19ac4d3239c91b677da2f5cc57
imRAYi <rayyan.khaled999@gmail.com>
2026-02-27 23:08:29 +0300

    Auto-update from Rayan Tool

--- upload.bat
+++ upload.bat
@@ -1,6 +1,6 @@
 @echo off
 git add .
-git commit -m "Auto-update images"
-git push
-echo Done! Your website is updating...
+git commit -m "Auto-update from Rayan Tool"
+git push origin main
+echo ✨ DONE! Rayletters.life is now updating...
 pause
